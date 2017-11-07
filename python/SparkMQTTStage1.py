e36986a927930d3e1d9dc8e9c5ad799f138ef827
AartiGorade <aartimali6689@gmail.com>
2017-11-07 15:19:05 -0500

    Added SparkMQTTStage1 and SparkMQTTStage2 file

---
+++ python/SparkMQTTStage1.py
@@ -0,0 +1,165 @@
+import os
+import socket
+import paho.mqtt.client as mqtt
+from collections import deque
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.mqtt import MQTTUtils
+from time import sleep
+from threading import Thread
+
+# MQTT client
+mqttc = None
+
+# Queue to store calculated average values
+queue = deque([])
+
+# counters to keep track of running sum and count to calculate average value
+sumAccum = 0
+countAccum = 0
+
+# window and sliding interval using for calculating average over each window of incoming Spar Stream
+windowInterval = 30
+slidingInterval = 15
+
+# Spark Broker details
+sparkBroker = "iot.eclipse.org"
+sparkPort = 1883
+
+# Calvin broker URI
+brokerUrl = "tcp://iot.eclipse.org:1883"
+# Topic pattern where temperature data is being sent
+topic = "testing/calvin/edu/rit/#"
+
+
+def connectToBroker(broker, port):
+    """
+    This is the function responsible for creating MQTT client and connecting to the give broker server on desired port
+    :param broker: broker server
+    :param port: port to connect to
+    :return: None
+    """
+
+    global mqttc
+    mqttc = mqtt.Client()
+    print "Trying to connect to broker..."
+    mqttc.connect(broker, port)
+    print "Successfully connected!!!"
+
+
+def addToQueue(rdd):
+    """
+    This is the function responsible for adding calculated average values into the queue
+    :param rdd: RDD containing calculated average values
+    :return: None
+    """
+
+    rddList = rdd.collect()
+    subList = [int(x[0]) for x in rddList]
+    global queue
+    queue.extend(subList)
+
+
+def publishFromQueue():
+    """
+    This is the function responsible for fetching data from queue and publishing it using MQTT
+    :return: None
+    """
+
+    global mqttc
+    global queue
+    mqttClient = mqttc
+    sparkTopic = "testing/spark/edu/rit/sensorAvg"
+
+    while True:
+        while not (queue):
+            sleep(slidingInterval)
+
+        data = queue.popleft()
+        print("Publishing new data = ", data)
+        mqttClient.publish(sparkTopic, data)
+
+
+def update(x):
+    """
+    Add the incoming new item in current sliding window interval into the sum
+    :param x: new value
+    :return: current average value
+    """
+    global sumAccum
+    global countAccum
+    sumAccum += x
+    countAccum += 1
+    return (sumAccum / countAccum)
+
+
+def reverseUpdate(x):
+    """
+    Remove item from old sliding window interval from current sum
+    :param x: old item from last window interval
+    :return: current average value
+    """
+    global sumAccum
+    global countAccum
+    sumAccum -= x
+    countAccum -= 1
+    return (sumAccum / countAccum)
+
+
+if __name__ == "__main__":
+    """
+    This is the main function responsible for calculating average of input data stream pe window and 
+    publishing calculated average values for Calvin client usage to perform further processing using 
+    Sensors or Actuators
+    """
+
+    # Load spark streaming mqtt package at runtime
+    SUBMIT_ARGS = "--packages org.apache.bahir:spark-streaming-mqtt_2.11:2.2.0 pyspark-shell"
+    os.environ["PYSPARK_SUBMIT_ARGS"] = SUBMIT_ARGS
+
+    # connect to Spark cluster "spark:cluster-host:port"
+    sc = SparkContext("spark://129.21.124.68:7077", appName="iotx")
+
+    print("Created Streaming context...")
+    ssc = StreamingContext(sc, 15)
+
+    # mandatory to store checkpointed data for Spark Streaming
+    ssc.checkpoint("/Users/Aarti/Documents/Fall2017/Code/CheckpointedData")
+
+    print("Creating MQTT stream...")
+    mqttStream = MQTTUtils.createStream(ssc, brokerUrl, topic)
+
+    # split incoming stream based on space
+    words = mqttStream.map(lambda line: line.split(" "))
+
+    # Convert unicode input into integer and store each value in pair format
+    pairs = words.map(lambda word: (int(word[0]), 1))
+
+    # lambda functions to calculate average using windowing technique
+    update_1 = lambda x, y: update(x)
+    reverseUpdate_1 = lambda x, y: reverseUpdate(x)
+
+    # Reduce last 30 seconds of data, every 15 seconds
+    windowedWordCounts = pairs.reduceByKeyAndWindow(update_1, reverseUpdate_1, windowInterval, slidingInterval)
+
+    # pprint is Action. prints first 60 items
+    windowedWordCounts.pprint(60)
+
+    # connect to broker
+    connectToBroker(sparkBroker, sparkPort)
+
+    # foreachRDD is Action. Add each RDD containing average values into the queue
+    windowedWordCounts.foreachRDD(addToQueue)
+
+    # create worker thread to fetch data from queue and publish it to broker using MQTT
+    worker = Thread(target=publishFromQueue)
+    worker.setDaemon(True)
+    worker.start()
+
+    # Start spark streaming jobs
+    print("\n\n SSC starting ...")
+    ssc.start()
+    print("\n\n SSC waiting for termination...")
+
+    # wait for 100 seconds before terminating Spark job execution
+    ssc.awaitTermination(1000)
---
+++ python/SparkMQTTStage2.py
@@ -0,0 +1,152 @@
+import os
+import json
+import threading
+import paho.mqtt.client as mqtt
+from collections import deque
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.mqtt import MQTTUtils
+from pyspark.streaming import DStream
+from time import sleep
+
+# MQTT client
+mqttc = None
+
+# Queue to store calculated average values
+queue = deque([])
+
+# Spark Broker details
+sparkBroker = "iot.eclipse.org"
+sparkPort = 1883
+sparkTopic = "testing/spark/edu/rit/sparkDag"
+
+# Calvin broker URI
+brokerUrl = "tcp://iot.eclipse.org:1883"
+# Calvin Topic pattern where temperature data is being sent
+topic = "testing/calvin/edu/rit/#"
+
+
+def connectToBroker(broker, port):
+    """
+    This is the function responsible for creating MQTT client and connecting to the give broker server on desired port
+    :param broker: broker server
+    :param port: port to connect to
+    :return: None
+    """
+
+    global mqttc
+    mqttc = mqtt.Client()
+    print "Trying to connect to broker..."
+    mqttc.connect(broker, port)
+    print "Successfully connected!!!"
+
+def extractDag():
+    """
+    Extract Directed Acyclic graph and offload to Edge in JSON format
+    :return:
+    """
+
+    return json.dumps(DStream.sparkDAG)
+
+def addToQueue():
+    """
+    This is the function responsible for adding extracted DAG JSON into the queue
+    :return: None
+    """
+    # Keep adding newly extracted DAG JSON in queue
+    while True:
+        global queue
+        queue.append(extractDag())
+        # wait for 5 seconds before queueing next DAG JSON
+        sleep(5)
+
+def publishFromQueue():
+    """
+    This is the function responsible for fetching data from queue and publishing it using MQTT
+    :return: None
+    """
+
+    global queue
+    # keep publishing DAG JSON
+    while True:
+        #wait for 15 seconds before publishing next DAG JSON
+        while not (queue):
+            sleep(15)
+
+        data = queue.popleft()
+        print("Publishing new data...", data)
+        mqttc.publish(sparkTopic, data)
+
+
+def printSparkDAG():
+    """
+    This is the function responsible to print extracted DAG in JSON readable format
+    :return: None
+    """
+
+    numEvents = len(DStream.sparkDAG)
+    print("[")
+    for i in range(0, numEvents):
+        event = DStream.sparkDAG[i]
+        print(" {")
+        print "   id: ", event["id"]
+        print "   rddType: ", event["rddType"]
+        print "   operationType: ", event["operationType"]
+        print "   operation: ", event["operation"]
+        print "   closure: ", event["closure"]
+        print "   additional Information: ", event["additionalInformation"]
+        print "   parent: ", event["parent"]
+        print "   uid: ", event["uid"]
+        print(" }")
+    print("]")
+
+
+if __name__ == "__main__":
+    '''
+    This is the main function responsible for collecting DAG from Spark and off loading to Calvin client
+    to perform evaluation
+    '''
+
+    # Load spark streaming mqtt package at runtime
+    SUBMIT_ARGS = "--packages org.apache.bahir:spark-streaming-mqtt_2.11:2.2.0 pyspark-shell"
+    os.environ["PYSPARK_SUBMIT_ARGS"] = SUBMIT_ARGS
+
+    # connect to Spark cluster "spark:cluster-host:port"
+    sc = SparkContext("spark://129.21.124.68:7077", appName="iotx")
+
+    print("Created Streaming context...")
+    ssc = StreamingContext(sc, 15)
+
+    # mandatory to store checkpointed data for Spark Streaming
+    ssc.checkpoint("/Users/Aarti/Documents/Fall2017/Code/CheckpointedData")
+
+    # broker URI
+    brokerUrl = "tcp://iot.eclipse.org:1883"
+    # topic or topic pattern where temperature data is being sent
+    topic = "testing/calvin/edu/rit/#"
+    # topic = "testing/calvin/edu/rit/const/sensor1"
+
+    print("Creating MQTT stream...")
+    mqttStream = MQTTUtils.createStream(ssc, brokerUrl, topic)
+
+    # split incoming stream based on space
+    words = mqttStream.map(lambda line: line.split(" "))
+
+    # Convert unicode input into integer and store each value in pair format
+    pairs = words.map(lambda word: (int(word[0]), 1))
+
+    # perform print action
+    pairs.pprint()
+
+    # print SPARK DAG in JSON readable format
+    printSparkDAG()
+
+    # connect to broker
+    connectToBroker(sparkBroker, sparkPort)
+
+    # Worker thread to perform operation to add newly extracted data into queue
+    t = threading.Timer(10.0, addToQueue)
+    t.start()
+
+    # Get DAG JSON from queue and publish to broker for Calvin usage
+    publishFromQueue()
